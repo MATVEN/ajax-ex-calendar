b58b64d2c560e1d87ad9fa06de34fe5eb875d2ed
MATVEN <mattiavena2993@gmail.com>
2019-11-21 17:38:10 +0100

    add function dynamic calendar

--- js/main.js
+++ js/main.js
@@ -15,12 +15,45 @@ Evidenziare le festività nella lista
 
 $(document).ready(function () {
 
-  var daysNumber = moment('01/01/2018', 'DD/MM/YYYY').daysInMonth();
+  var month = 1;
 
-  for (var i = 0; i < daysNumber; i++) {
+  function generaMese(mese) {
+    //dinamicizzo
 
-    var currentDay = moment('2018-01-' + i).format('DD dddd')
-    console.log(currentDay);
+    $.ajax({
+
+      url: "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0" + (month-1),
+      method: "GET",
+      success: function (data) {
+
+        var daysNumber = moment('01/' + month + '/2018', 'DD/MM/YYYY').daysInMonth();
+
+        for (var i = 1; i <= daysNumber; i++) {
+
+          var currentDate = '2018-' + month + '-' + i;
+
+          var dateForm = moment(currentDate).format("YYYY-MM-DD");
+
+          var currentDay = moment('2018-' + month + '-' + i).format('DD dddd');
+          console.log(currentDay);
+          $(".calendar").append('<li data-date="' + dateForm + '">'+ currentDay +'</li>');
+
+        }
+
+        for (var i = 0; i < data.response.length; i++) {
+          var holidayDate = data.response[i].date;
+          console.log(data.response[i].date);
+          var holidayName = data.response[i].name;
+          console.log(data.response[i].name);
+
+          $(".calendar [data-date='" + holidayDate + "']").css("color", "red").append(" " + holidayName)
+        }
+
+      },
+
+    });
   }
+
+  generaMese(1);
 
 });
